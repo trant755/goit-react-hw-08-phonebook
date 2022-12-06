afb006ffb03e254c4b57112515edf1e2e639f29f
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2022-12-06 15:01:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,9 @@
 import { Box } from './Box';
-import { useEffect } from 'react';
 import { Section } from './Seaction';
 import { ContactForm } from './PhonebookForm';
 import { Contacts } from './Contacts';
-import { useSelector } from 'react-redux';
-
-const LS_KEY = 'savedContacts';
 
 export const App = () => {
-  // const savedContacts = localStorage.getItem(LS_KEY);
-  const contacts = useSelector(state => state.contacts.contacts);
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <Box p={4}>
       <Section title={'Phonebook'}>
